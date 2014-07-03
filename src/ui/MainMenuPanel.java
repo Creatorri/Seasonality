4da1fc52d09d08fa227359979cc587afaf2c5986
Creatorri <torridon@live.com>
2014-07-03 19:08:17 -0400

    All images incorporated; time for bug discovery and fixing

--- src/ui/MainMenuPanel.java
+++ src/ui/MainMenuPanel.java
@@ -19,13 +19,13 @@ public class MainMenuPanel extends JPanel {
         Seasonality.buttons.get(Seasonality.buttons.size() - 1).setVisible(true);
         Seasonality.buttons.get(Seasonality.buttons.size() - 1).addListener(Seasonality.mbi);
 
-        Seasonality.buttons.add(new MButton(750 / 2, 10, 100, 50, "Play Easy Mode", this));
-        Seasonality.placeX.add(0.5);
-        Seasonality.placeY.add(0.55);
-        Seasonality.sizeX.add(0.2);
-        Seasonality.sizeY.add(0.1);
-        Seasonality.buttons.get(Seasonality.buttons.size() - 1).setVisible(true);
-        Seasonality.buttons.get(Seasonality.buttons.size() - 1).addListener(Seasonality.mbi);
+//        Seasonality.buttons.add(new MButton(750 / 2, 10, 100, 50, "Play Easy Mode", this));
+//        Seasonality.placeX.add(0.5);
+//        Seasonality.placeY.add(0.55);
+//        Seasonality.sizeX.add(0.2);
+//        Seasonality.sizeY.add(0.1);
+//        Seasonality.buttons.get(Seasonality.buttons.size() - 1).setVisible(true);
+//        Seasonality.buttons.get(Seasonality.buttons.size() - 1).addListener(Seasonality.mbi);
 
         Seasonality.buttons.add(new MButton(750 / 2, 10, 100, 50, "Quit", this));
         Seasonality.placeX.add(0.5);
